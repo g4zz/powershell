41d23bbf66822a09647b901980c49702c4ba14ad
g4zz <garath.emms@gmail.com>
2019-08-13 11:42:32 +0100

    Sanitized all-staff.ps1

--- all-staff.ps1
+++ all-staff.ps1
@@ -1,9 +1,11 @@
 ﻿# Powershell to get all staff details and email them
 
 # -- Variables
-$smtpServer="int.scottlogic.co.uk" # Address of SMTP server
-$emailaddress="gemms@scottlogic.com" # Email address to send the export
-$from="gemms@scottlogic.com" # Address email will be from
+$smtpServer="" # Address of SMTP server
+$emailaddress="" # Email address to send the export
+$from="" # Address email will be from
+$ou="'OU=Newcastle,OU=Staff,DC=scottlogic,DC=co,DC=uk'" # Organisational Unit you wish to search
+
 $textEncoding = [System.Text.Encoding]::UTF8
 
 Import-Module ActiveDirectory
@@ -19,19 +21,10 @@ $subject="All Staff Report" # Email subject
 # -- Split information into 4 seperate tables, one for each office
 # -- Sorting by Manager,Name
 # -- Converting to HTML and then outputting as a string for putting into the email body
-$newcastle=(get-aduser -Filter * -SearchBase 'OU=Newcastle,OU=Staff,DC=scottlogic,DC=co,DC=uk' -Properties * | select Name,Title,@{n="Manager";e={(Get-AdUser -Identity $_.Manager -Properties displayName).displayName}},Office | Sort-Object -Property Manager,Name | ConvertTo-Html -Head $style) | Out-String
-$bristol=(get-aduser -Filter * -SearchBase 'OU=Bristol,OU=Staff,DC=scottlogic,DC=co,DC=uk' -Properties * | select Name,Title,@{n="Manager";e={(Get-AdUser -Identity $_.Manager -Properties displayName).displayName}},Office | Sort-Object -Property Manager,Name | ConvertTo-Html -Head $style) | Out-String
-$edinburgh=(get-aduser -Filter * -SearchBase 'OU=Edinburgh,OU=Staff,DC=scottlogic,DC=co,DC=uk' -Properties * | select Name,Title,@{n="Manager";e={(Get-AdUser -Identity $_.Manager -Properties displayName).displayName}},Office | Sort-Object -Property Manager,Name | ConvertTo-Html -Head $style) | Out-String
-$london=(get-aduser -Filter * -SearchBase 'OU=London,OU=Staff,DC=scottlogic,DC=co,DC=uk' -Properties * | select Name,Title,@{n="Manager";e={(Get-AdUser -Identity $_.Manager -Properties displayName).displayName}},Office | Sort-Object -Property Manager,Name | ConvertTo-Html -Head $style) | Out-String
+$allstaff=(get-aduser -Filter * -SearchBase $ou -Properties * | select Name,Title,@{n="Manager";e={(Get-AdUser -Identity $_.Manager -Properties displayName).displayName}},Office | Sort-Object -Property Manager,Name | ConvertTo-Html -Head $style) | Out-String
+
 # -- Putting information into the $body variable for email
-$body="<p>Newcastle</p>
-    <p>$newcastle</p>
-    <p>Bristol</p>
-    <p>$bristol</p>
-    <p>Edinburgh</p>
-    <p>$edinburgh</p>
-    <p>London</p>
-    <p>$london</p>"
+$body="<p>All Staff</p><p>$allstaff</p>    "
 
 # -- Send email
 Send-Mailmessage -smtpServer $smtpServer -from $from -to $emailaddress -subject $subject -body $body -bodyasHTML -Encoding $textEncoding
