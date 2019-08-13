b6dc6bf6498ec90743030c63c02c19505d8eaf37
g4zz <garath.emms@gmail.com>
2019-08-13 11:47:00 +0100

    Sanatized all-staff.ps1

--- all-staff.ps1
+++ all-staff.ps1
@@ -13,14 +13,10 @@ Import-Module ActiveDirectory
 # -- Generate email
 $subject="All Staff Report" # Email subject
 
-# -- Get information template
-# -- @{n="Manager";e={(Get-AdUser -Identity $_.Manager -Properties displayName).displayName}} is an expression used to get manager's displayname
-
-#get-aduser -Filter * -SearchBase 'OU=Newcastle,OU=Staff,DC=scottlogic,DC=co,DC=uk' -Properties * | select Name,Title,@{n="Manager";e={(Get-AdUser -Identity $_.Manager -Properties displayName).displayName}},Office | Sort-Object -Property Manager,Name
-
 # -- Split information into 4 seperate tables, one for each office
 # -- Sorting by Manager,Name
 # -- Converting to HTML and then outputting as a string for putting into the email body
+# -- @{n="Manager";e={(Get-AdUser -Identity $_.Manager -Properties displayName).displayName}} is an expression used to get manager's displayname
 $allstaff=(get-aduser -Filter * -SearchBase $ou -Properties * | select Name,Title,@{n="Manager";e={(Get-AdUser -Identity $_.Manager -Properties displayName).displayName}},Office | Sort-Object -Property Manager,Name | ConvertTo-Html -Head $style) | Out-String
 
 # -- Putting information into the $body variable for email
